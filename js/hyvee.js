d5757c7e4dcd350efc42df2c2e37d189628d4a87
Cesar Castro <cesar.castro@revelo.com.br>
2020-11-29 14:40:51 -0300

    Fix responsiveness and add responsive menu

--- js/hyvee.js
+++ js/hyvee.js
@@ -10,4 +10,20 @@ function handleDefinitionLists() {
     });
 }
 
+function handleHamburgerMenu() {
+    const header = document.querySelector("header");
+    const navMenu = header.querySelector("nav");
+    const iconHamburger = header.querySelector(".hamburger");
+    const iconClose = header.querySelector(".close");
+
+    iconHamburger.addEventListener("click", () => {
+        navMenu.classList.add("visible");
+    });
+
+    iconClose.addEventListener("click", () => {
+        navMenu.classList.remove("visible");
+    })
+}
+
 handleDefinitionLists();
+handleHamburgerMenu();
